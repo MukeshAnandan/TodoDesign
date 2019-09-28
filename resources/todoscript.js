1f89dae1110f56c63625c6026c3a002f4d85c0bb
MukeshAnandan <mukesh@ideas2it.com>
2019-09-28 16:51:05 +0530

    additional notes and sub steps

--- resources/todoscript.js
+++ resources/todoscript.js
@@ -1,7 +1,13 @@
+/**
+ * used to open the side panel 
+ */
 function openNav() {
   document.getElementById("mysubbar").style.width = "325px";
 }
 
+/**
+ * used to close the side panel
+ */
 function closeNav() {
   document.getElementById("mysubbar").style.width = "0";
 }
@@ -20,53 +26,70 @@ var strikedNote = [];
 var strikeName;
 var strikeID;
 var strikestatus;
+var subNotesCount = 1;
 
 
 
-var newtask = document.getElementById("taskinput");
-newtask.addEventListener("keyup", createNewTask);
+var newtask = $("#taskinput");
+newtask.keyup(createNewTask);
 console.log(document.getElementById("taskinput").value);
 var taskAssign = document.getElementsByClassName("addlists")[0];
 var tasktitle = document.getElementsByClassName("tasktitle")[0];
 var taskAttributes = document.getElementsByClassName("taskAttributes")[0];
 var hideOldNotes = document.getElementById("newTaskDef")[0];
 
+/**
+ * 
+ * @param {*} e  - used to call the function which is used to 
+ * add the new task while kekup.
+ */
 function createNewTask(e) {
     console.log("CREATE NEW");
-    if(e.keyCode == 13 && newtask.value != "") {
+    if(e.keyCode == 13 && newtask.val() != "") {
         // addEventListener("click",getTaskTitle());
         addTask();
     }
 }
 
+/**
+ * This function is used to create a new task and append to the old task.
+ */
 function addTask() {
     let list = {};
     var taskName = document.createElement("span");
     var taskDiv = document.createElement("div");
-    var nextTask = newtask.value;
+    var nextTask = newtask.val()
     var taskList = document.getElementById("taskList");
-    list.listname = newtask.value;
+    list.listname = newtask.val();
     list.tasks = [];
     taskName.innerHTML = list.listname;
+    taskDiv.className = "taskPointer"
     taskDiv.innerHTML= '<i class="iconsize listicon"></i>';
-    taskDiv.appendChild(taskName);
-    taskList.appendChild(taskDiv);
+    taskDiv.append(taskName);
+    taskList.append(taskDiv);
     lists.push(list);
     list.listid = lists.length - 1;
     console.log(list.listid);
     console.log(lists.length - 1);
-    newtask.value ="";
+    newtask.val("");
     taskDiv.addEventListener("click",getTaskTitle.bind(list));
     taskDiv.addEventListener("click",getTask.bind(list));
     listid  = list.id;
 }
 
+/**
+ * used to get the task name and append in the task title 
+ */
 function getTaskTitle() {
     tasktitle.innerText = this.listname;
     document.getElementById("newTaskDef").innerHTML = "";
 }
 
-
+/**
+ * 
+ * @param {*} listid - used to get the entire task for the 
+ * specific listID and display that while switching.
+ */
 function switchCurrentNotes(listid) {
   console.log(listid);
   var list =  lists[listid];
@@ -76,8 +99,12 @@ function switchCurrentNotes(listid) {
   var oldtasks = list.tasks;
    for(i = 0;i <= oldtasks.length-1; i++) {
         var taskName = document.createElement("span");
+        var br = document.createElement("br");
+        var subCount = document.createElement("p");
         var taskDiv = document.createElement("div");
+       // var subDiv = document.createElement("div");
         taskDiv.id = divisionId++;
+        //subDiv.id = divisionId++;
         console.log(taskDiv.id);
         var taskList = document.getElementById("newTaskDef");
         /*if(strikedNote[i].strikeName === oldtasks[i].notesname && 
@@ -88,25 +115,33 @@ function switchCurrentNotes(listid) {
             ele.style.setProperty("text-decoration", "line-through");  
         }*/
         taskName.innerHTML = oldtasks[i].notesname ;
+        subCount.innerHTML = oldtasks[i].subTaskCount;
         oldnote.notesname = oldtasks[i].notesname ;
         var input = document.createElement("INPUT");
         input.type = "checkbox";
         taskDiv.className = "newtask";
-        
+        subCount.className = "countDiv";
+        taskName.className = "taskNameSpan";
+        //subDiv.className = "countDiv";
+        //subDiv.appendChild(subCount);
         taskDiv.appendChild(input);
         taskDiv.appendChild(taskName);
+        
+        if(oldtasks[i].subTaskCount != 0){
+            taskDiv.appendChild(br);
+            taskDiv.appendChild(subCount);
+        }
         taskList.appendChild(taskDiv);
         console.log("end....................");
         taskName.addEventListener("click",getNotesTitle.bind(oldtasks[i]));
-        taskName.addEventListener("click",changeName.bind(oldtasks[i]));
-        
-        
-        
-        
+        taskName.addEventListener("click",changeName.bind(oldtasks[i]));    
    }
 
 }
 
+/**
+ * used to get the listid for the particular task.
+ */
 function getTask() {
     listid = this.listid;
     console.log(listid);
@@ -118,6 +153,11 @@ newtaskInfo.addEventListener("keyup", createNewNote);
 
 var NoteAssign = document.getElementsByClassName("newtaskDetail")[0];
 
+/**
+ * 
+ * @param {*} e - used to call the function for creating new notes
+ * for the particular task.
+ */
 function createNewNote(e) {
     console.log("CREATE NEW");
     if(e.keyCode == 13 && newtaskInfo.value != "") {
@@ -125,12 +165,18 @@ function createNewNote(e) {
     }
 }
 
+/**
+ * used to create the new notes for the particular task and 
+ * append that to the old notes
+ */
 function addNote() {
     var list =  lists[listid];
+    var note = list.tasks[taskId];
     console.log(lists[listid]);
     var notes = {};
     notes.subnote = [];
     notes.additionalNote = "";
+    notes.subTaskCount = 0;
     var taskName = document.createElement("span");
     var taskDiv = document.createElement("div");
     taskDiv.id = divId++;
@@ -139,20 +185,24 @@ function addNote() {
     notes.notesname = newtaskInfo.value;
     var taskList = document.getElementById("newTaskDef");
     taskName.innerHTML = newtaskInfo.value;
+    
     var input = document.createElement("INPUT");
     input.type = "checkbox";
     taskDiv.className = "newtask";
     taskDiv.appendChild(input);
     taskDiv.appendChild(taskName);
+   
     taskList.appendChild(taskDiv);
     newtaskInfo.value ="";
     notes.notesid = list.tasks.length;
     list.tasks.push(notes);
     console.log("notesID incr"+notes.notesid);
     //createStep();
+    
     taskName.addEventListener("click",getNotesTitle.bind(notes));
     taskName.addEventListener("click",changeName.bind(notes));
     input.addEventListener("click", strikeFunction.bind(taskDiv));
+    
     //taskDiv.addEventListener("click",getTask.bind(notes));
 } 
 
@@ -163,6 +213,10 @@ newSubNotes.addEventListener("keyup", createSubNote);
 
 var subNoteAssign = document.getElementsByClassName("subnotes")[0];
 
+/**
+ * 
+ * @param {*} e -used to call the subnote function while key up
+ */
 function createSubNote(e) {
     console.log("CREATE NEW subnotes");
     if(e.keyCode == 13 && newSubNotes.value != "") {
@@ -170,11 +224,16 @@ function createSubNote(e) {
     }
 }
 
+/**
+ * used to create the sub notes for the particular note 
+ * and append that to the old notes.
+ */
 function subNote() {
+    
     console.log("taskId"+taskId)
     var list = lists[listid];
     var note = list.tasks[taskId];
-    
+    //subNotesCount = note.subTaskCount;
     console.log(list.tasks[taskId]);
     var subnotes = {};
     var taskName = document.createElement("span");
@@ -191,37 +250,53 @@ function subNote() {
     notesDiv.appendChild(inputType);
     notesDiv.appendChild(taskName);
     taskList.appendChild(notesDiv);
+    note.subTaskCount = note.subnote.length+1;
     newSubNotes.value ="";
     console.log("before push");
     note.subnote.push(subnotes);
     console.log("after push");
     console.log(lists[listid].tasks);
+    
     inputType.addEventListener("click", strikeFunction.bind(notesDiv));
+    document.getElementById("newTaskDef").innerHTML = "";
+    switchCurrentNotes(listid);
 } 
 
 var additionalNotes= document.getElementById("additional");
 additionalNotes.addEventListener("keyup", additionalNoteForm);
 
+/**
+ * 
+ * @param {*} e -used to call the function that creating
+ * additional notes for the particular task.
+ */
 function additionalNoteForm(e) {
     if(e.keyCode == 13 && additionalNotes.value != "") {
         console.log("aDD 1");
         additionalSubNotes();
+        document.getElementById("newTaskDef").innerHTML = "";
+        switchCurrentNotes(listid);
+        console.log("aaddddddddddd endzzzzzzzzzzzz");
     }
 } 
 
+/**
+ * used to create the additional subnotes for the 
+ * particular task.
+ */
 function additionalSubNotes() {
     console.log("aDD 2");
     var list = lists[listid];
     var note = list.tasks[taskId];
     note.additionalNote = additionalNotes.value;
     console.log("aDD  END");
-
 }
 
-
-
-
-
+/**
+ * used to get the notes title and append it as task title
+ * and get the old sub notes and additional notes while 
+ * switching.
+ */
 function getNotesTitle() {
 
     console.log("notes id"+this.notesid);
@@ -230,13 +305,16 @@ function getNotesTitle() {
     taskAttributes.value = this.notesname;
     document.getElementById("subNotes").innerHTML = "";
     document.getElementById("additional").value = "";
-    
+    subNotesCount = 1;
     viewCurrentsubNotes(taskId);
     viewAdditionalNotes(taskId);
     console.log("tommy");
     
 }
 
+/**
+ * used to rename the task name.
+ */
 function changeName() {
     console.log("tommy son");
     taskId = this.notesid;
@@ -250,6 +328,9 @@ function changeName() {
                 changeNoteName();
             }
         }
+        /**
+         * used to change the old name for the task from the sub task menu.
+         */
         function changeNoteName() {
             console.log("tommy in");
             var list =  lists[listid];
@@ -273,6 +354,11 @@ function changeName() {
         }
 }    
 
+/**
+ * 
+ * @param {*} taskId -used to get the old additional note for the 
+ * particular task and list by using task Id.
+ */
 function viewAdditionalNotes(taskId) {
   var list = lists[listid];
   var note = list.tasks[taskId];
@@ -281,6 +367,10 @@ function viewAdditionalNotes(taskId) {
   add_text.value = additionaltext;
 }
 
+/**
+ * 
+ * @param {*} taskId -used to get the old subnotes while switching.
+ */
 function viewCurrentsubNotes(taskId) {
   console.log(taskId);
   var list = lists[listid];
@@ -290,27 +380,27 @@ function viewCurrentsubNotes(taskId) {
   var additionaltext = note.additionalNote;
   
    for(i = 0;i <= oldsubnotes.length-1; i++) {
-    var taskName = document.createElement("span");
-    var notesDiv = document.createElement("div");
-    notesDiv.id = divisionId++;
-    var taskList = document.getElementById("subNotes");
-    console.log(oldsubnotes[i]);
-    taskName.innerHTML = oldsubnotes[i].subnotesname ;
-    var inputType = document.createElement("INPUT");
-    inputType.type = "checkbox";
-    notesDiv.className = "newNotes";
-    notesDiv.appendChild(inputType);
-    notesDiv.appendChild(taskName);
-    taskList.appendChild(notesDiv);
-    
-    inputType.addEventListener("click", strikeFunction.bind(notesDiv));  
+        var taskName = document.createElement("span");
+        var notesDiv = document.createElement("div");
+        notesDiv.id = divisionId++;
+        var taskList = document.getElementById("subNotes");
+        console.log(oldsubnotes[i]);
+        taskName.innerHTML = oldsubnotes[i].subnotesname ;
+        var inputType = document.createElement("INPUT");
+        inputType.type = "checkbox";
+        notesDiv.className = "newNotes";
+        notesDiv.appendChild(inputType);
+        notesDiv.appendChild(taskName);
+        taskList.appendChild(notesDiv);
+        
+        inputType.addEventListener("click", strikeFunction.bind(notesDiv));  
    }
 
 }
 
-
-
-
+/**
+ * used to strike the name of the task and subnotes while calling.
+ */
 function strikeFunction() {
     
     var strikedDetail = {};
@@ -323,6 +413,10 @@ function strikeFunction() {
         strikedNote.push(strikedDetail);
 }
 
+/**
+ * used to add the sub steps for the paritcular task and append that
+ * to the old steps.
+ */
 function createStep() {
     var input = document.createElement("INPUT");
     input.type = "checkbox";
@@ -331,24 +425,3 @@ function createStep() {
     input.appendChild(taskName);
     addSteps.appendChild(input);   
 }
-
-
-
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
